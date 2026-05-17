1449969c54d3dd9aa74c9e8ad80d6b642acbb0c8
LevelUpSites <williamhenryeguizabal@gmail.com>
2026-05-17 12:13:47 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // animacion
 
 const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-        } else {
-            entry.target.classList.remove('visible');
-        }
-    });
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
 }, { threshold: 0.1 });
 
 document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
